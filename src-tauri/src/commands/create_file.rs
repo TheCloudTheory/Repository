fbf7ef0e5a67208b556ba635a015f2654544715e
Kamil Mrzygłód <kamil@thecloudtheory.com>
2025-02-14 13:45:16 +0100

    Simple sanitizing of the file name

--- src-tauri/src/commands/create_file.rs
+++ src-tauri/src/commands/create_file.rs
@@ -5,6 +5,11 @@ pub fn create_file(file_name: &str) -> Result<(), String> {
         return Err("File already exists".to_string());
     }
 
+    // The actual name of the file could be different than the name
+    // of an entry displayed in the UI. By convention, we will replace
+    // spaces with hyphens and make the name lowercase
+    let file_name = file_name.replace(" ", "-").to_lowercase();
+
     // We need to create the file
     std::fs::File::create(format!("{}.md", &file_name)).unwrap();
 
